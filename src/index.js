bc95fb226d0fc9ca6abdb79e221ff95c16a01d65
anthony <anthony.troupenat@studylink.fr>
2019-01-28 17:13:21 +0100

    add rollup config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const convertLineBreaks = require('./convertLineBreaks');
 const isBrowser = require('./isBrowser');
 const browserRedirect = require('./browserRedirect');
 
-module.exports = {
+export default {
   convertLineBreaks,
   isBrowser,
   browserRedirect,
